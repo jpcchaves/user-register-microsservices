84a9e8d3fac3d91375240a60b8651d9e52e34125
jpcchaves <jpcchaves@outlook.com>
2025-01-12 12:12:01 -0300

    add validation exception

---
+++ orchestrator/src/main/java/com/jpcchaves/orchestrator/core/exception/ValidationException.java
@@ -0,0 +1,8 @@
+package com.jpcchaves.orchestrator.core.exception;
+
+public class ValidationException extends RuntimeException {
+
+    public ValidationException(String message) {
+        super(message);
+    }
+}
