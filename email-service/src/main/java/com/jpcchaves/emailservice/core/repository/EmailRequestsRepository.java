a24549feae33a43104b488618e23972ee2566643
jpcchaves <jpcchaves@outlook.com>
2025-01-12 18:31:39 -0300

    fix transaction id parameter type

--- email-service/src/main/java/com/jpcchaves/emailservice/core/repository/EmailRequestsRepository.java
+++ email-service/src/main/java/com/jpcchaves/emailservice/core/repository/EmailRequestsRepository.java
@@ -2,14 +2,15 @@ package com.jpcchaves.emailservice.core.repository;
 
 import com.jpcchaves.emailservice.core.model.EmailRequests;
 
-import java.util.Optional;
 import jdk.jfr.Registered;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 @Registered
 public interface EmailRequestsRepository extends JpaRepository<EmailRequests, Long> {
-    Boolean existsByIdAndTransactionId(Long id, Long transactionId);
+    Boolean existsByIdAndTransactionId(Long id, String transactionId);
 
-    Optional<EmailRequests> findByIdAndTransactionId(Long id, Long transactionId);
+    Optional<EmailRequests> findByIdAndTransactionId(Long id, String transactionId);
 }
