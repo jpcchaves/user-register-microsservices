868bf0a5b760d0ceffc5472a619d8ba4bd047a26
jpcchaves <jpcchaves@outlook.com>
2025-01-25 22:46:24 -0300

    implemented inheritor to handle success strategy

---
+++ auth-service/src/main/java/com/jpcchaves/authservice/core/strategy/EndRegistrationSuccessStrategy.java
@@ -0,0 +1,23 @@
+package com.jpcchaves.authservice.core.strategy;
+
+import com.jpcchaves.authservice.core.enums.ESagaStatus;
+import com.jpcchaves.authservice.core.model.Event;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Component;
+
+@Component
+public class EndRegistrationSuccessStrategy extends EndRegistrationSagaStrategy {
+
+    private static final Logger log = LoggerFactory.getLogger(EndRegistrationSuccessStrategy.class);
+
+
+    EndRegistrationSuccessStrategy() {
+        super(ESagaStatus.SUCCESS);
+    }
+
+    @Override
+    public void handleEvent(Event event) {
+        log.info("Received event in success strategy {}", event);
+    }
+}
