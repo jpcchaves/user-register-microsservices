6e637a59bd460be4d70b8fa20f1f18644e081d98
jpcchaves <jpcchaves@outlook.com>
2025-01-25 21:38:17 -0300

    add user mapper

---
+++ auth-service/src/main/java/com/jpcchaves/authservice/core/util/UserMapper.java
@@ -0,0 +1,19 @@
+package com.jpcchaves.authservice.core.util;
+
+import com.jpcchaves.authservice.core.model.User;
+import com.jpcchaves.authservice.core.model.dto.UserRegisterDTO;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class UserMapper {
+
+    public User toUser(UserRegisterDTO registerDTO) {
+        return User.builder()
+                .firstName(registerDTO.getFirstName())
+                .lastName(registerDTO.getLastName())
+                .email(registerDTO.getEmail())
+                .password(registerDTO.getPassword())
+                .build();
+    }
+}
