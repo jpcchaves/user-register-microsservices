63f9a9b0e6430e4cf1798043450d3b123665893e
jpcchaves <jpcchaves@outlook.com>
2025-01-25 21:05:51 -0300

    add user register and response dtos

---
+++ auth-service/src/main/java/com/jpcchaves/authservice/core/model/dto/UserRegisterDTO.java
@@ -0,0 +1,64 @@
+package com.jpcchaves.authservice.core.model.dto;
+
+import java.io.Serial;
+import java.io.Serializable;
+
+public class UserRegisterDTO implements Serializable {
+
+    @Serial private static final long serialVersionUID = 5557311891236268271L;
+
+    private String firstName;
+    private String lastName;
+    private String email;
+    private String password;
+    private String confirmPassword;
+    private String phoneNumber;
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getConfirmPassword() {
+        return confirmPassword;
+    }
+
+    public void setConfirmPassword(String confirmPassword) {
+        this.confirmPassword = confirmPassword;
+    }
+
+    public String getPhoneNumber() {
+        return phoneNumber;
+    }
+
+    public void setPhoneNumber(String phoneNumber) {
+        this.phoneNumber = phoneNumber;
+    }
+}
