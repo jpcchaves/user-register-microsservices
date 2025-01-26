48ef2fcc6e5804153d38de5c5c80bc5518c438fd
jpcchaves <jpcchaves@outlook.com>
2025-01-25 22:46:28 -0300

    implemented inheritor to handle fail strategy

---
+++ auth-service/src/main/java/com/jpcchaves/authservice/core/strategy/EndRegistrationFailStrategy.java
@@ -0,0 +1,22 @@
+package com.jpcchaves.authservice.core.strategy;
+
+import com.jpcchaves.authservice.core.enums.ESagaStatus;
+import com.jpcchaves.authservice.core.model.Event;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Component;
+
+@Component
+public class EndRegistrationFailStrategy extends EndRegistrationSagaStrategy {
+
+    private static final Logger log = LoggerFactory.getLogger(EndRegistrationFailStrategy.class);
+
+    public EndRegistrationFailStrategy() {
+        super(ESagaStatus.FAIL);
+    }
+
+    @Override
+    public void handleEvent(Event event) {
+        log.info("Received event in fail strategy {}", event);
+    }
+}
