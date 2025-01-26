083e908815076db64eed124df3eb3afb6855f76b
jpcchaves <jpcchaves@outlook.com>
2025-01-25 21:38:12 -0300

    add user response dto builder

--- auth-service/src/main/java/com/jpcchaves/authservice/core/model/dto/UserResponseDTO.java
+++ auth-service/src/main/java/com/jpcchaves/authservice/core/model/dto/UserResponseDTO.java
@@ -7,6 +7,18 @@ public class UserResponseDTO {
     private String email;
     private String phoneNumber;
 
+    public UserResponseDTO(Builder builder) {
+        this.id = builder.id;
+        this.firstName = builder.firstName;
+        this.lastName = builder.lastName;
+        this.email = builder.email;
+        this.phoneNumber = builder.phoneNumber;
+    }
+
+    public static Builder builder() {
+        return new Builder();
+    }
+
     public Long getId() {
         return id;
     }
@@ -45,5 +57,42 @@ public class UserResponseDTO {
 
     public void setPhoneNumber(String phoneNumber) {
         this.phoneNumber = phoneNumber;
+    }
+
+    public static class Builder {
+        private Long id;
+        private String firstName;
+        private String lastName;
+        private String email;
+        private String phoneNumber;
+
+        public Builder id(Long id) {
+            this.id = id;
+            return this;
+        }
+
+        public Builder firstName(String firstName) {
+            this.firstName = firstName;
+            return this;
+        }
+
+        public Builder lastName(String lastName) {
+            this.lastName = lastName;
+            return this;
+        }
+
+        public Builder email(String email) {
+            this.email = email;
+            return this;
+        }
+
+        public Builder phoneNumber(String phoneNumber) {
+            this.phoneNumber = phoneNumber;
+            return this;
+        }
+
+        public UserResponseDTO build() {
+            return new UserResponseDTO(this);
+        }
     }
 }
