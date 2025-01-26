7fcb9b67500dd8d5d85f5d22cdf8b627c6c9899e
jpcchaves <jpcchaves@outlook.com>
2025-01-25 21:36:37 -0300

    add import sql

---
+++ auth-service/src/main/resources/import.sql
@@ -0,0 +1,3 @@
+INSERT INTO roles (id, name) VALUES (nextval('seq_role'), 'ROLE_USER');
+INSERT INTO roles (id, name) VALUES (nextval('seq_role'), 'ROLE_ADMIN');
+INSERT INTO roles (id, name) VALUES (nextval('seq_role'), 'ROLE_TEST');
