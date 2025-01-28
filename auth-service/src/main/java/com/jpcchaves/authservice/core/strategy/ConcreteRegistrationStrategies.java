c37380b97109f73bd878fd549c454c7f9f67cf2b
jpcchaves <jpcchaves@outlook.com>
2025-01-28 15:58:50 -0300

    make strategiesMap private final, initialize it thru constructor to make it immutable and add nullcheck in getstrategy method

--- auth-service/src/main/java/com/jpcchaves/authservice/core/strategy/ConcreteRegistrationStrategies.java
+++ auth-service/src/main/java/com/jpcchaves/authservice/core/strategy/ConcreteRegistrationStrategies.java
@@ -4,21 +4,33 @@ import com.jpcchaves.authservice.core.enums.ESagaStatus;
 
 import org.springframework.stereotype.Component;
 
-import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
+import java.util.function.Function;
+import java.util.stream.Collectors;
 
 @Component
 public class ConcreteRegistrationStrategies {
 
-    public Map<ESagaStatus, AbstractEndRegistrationSagaStrategy> strategiesMap = new HashMap<>();
+    private final Map<ESagaStatus, AbstractEndRegistrationSagaStrategy> strategiesMap;
 
     public ConcreteRegistrationStrategies(List<AbstractEndRegistrationSagaStrategy> strategies) {
-        strategies.forEach(strategy -> strategiesMap.put(strategy.getStrategyName(), strategy));
+        this.strategiesMap =
+                strategies.stream()
+                        .collect(
+                                Collectors.toMap(
+                                        AbstractEndRegistrationSagaStrategy::getStrategyName,
+                                        Function.identity()));
     }
 
     public AbstractEndRegistrationSagaStrategy getStrategy(String endSagaStatus) {
         ESagaStatus esagaStatus = ESagaStatus.valueOf(endSagaStatus);
+        AbstractEndRegistrationSagaStrategy strategy = strategiesMap.get(esagaStatus);
+
+        if (strategy == null) {
+            throw new IllegalArgumentException("Unknown end saga status: " + endSagaStatus);
+        }
+
         return strategiesMap.get(esagaStatus);
     }
 }
