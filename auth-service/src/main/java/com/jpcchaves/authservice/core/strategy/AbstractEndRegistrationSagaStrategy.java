f963ae383e52f9a64607c8f3aabb3676c350d902
jpcchaves <jpcchaves@outlook.com>
2025-01-28 15:54:22 -0300

    make strategyName private final and initialize it thru constructor to ensure immutability

--- auth-service/src/main/java/com/jpcchaves/authservice/core/strategy/AbstractEndRegistrationSagaStrategy.java
+++ auth-service/src/main/java/com/jpcchaves/authservice/core/strategy/AbstractEndRegistrationSagaStrategy.java
@@ -4,11 +4,15 @@ import com.jpcchaves.authservice.core.enums.ESagaStatus;
 import com.jpcchaves.authservice.core.model.Event;
 
 public abstract class AbstractEndRegistrationSagaStrategy {
-    public ESagaStatus strategyName;
+    private final ESagaStatus strategyName;
 
-    public abstract void handleEvent(Event event);
-
-    public AbstractEndRegistrationSagaStrategy(ESagaStatus strategyName) {
+    protected AbstractEndRegistrationSagaStrategy(ESagaStatus strategyName) {
         this.strategyName = strategyName;
     }
+
+    public ESagaStatus getStrategyName() {
+        return strategyName;
+    }
+
+    public abstract void handleEvent(Event event);
 }
--- auth-service/src/main/java/com/jpcchaves/authservice/core/strategy/ConcreteRegistrationStrategies.java
+++ auth-service/src/main/java/com/jpcchaves/authservice/core/strategy/ConcreteRegistrationStrategies.java
@@ -1,10 +1,12 @@
 package com.jpcchaves.authservice.core.strategy;
 
 import com.jpcchaves.authservice.core.enums.ESagaStatus;
+
+import org.springframework.stereotype.Component;
+
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
-import org.springframework.stereotype.Component;
 
 @Component
 public class ConcreteRegistrationStrategies {
@@ -12,7 +14,7 @@ public class ConcreteRegistrationStrategies {
     public Map<ESagaStatus, AbstractEndRegistrationSagaStrategy> strategiesMap = new HashMap<>();
 
     public ConcreteRegistrationStrategies(List<AbstractEndRegistrationSagaStrategy> strategies) {
-        strategies.forEach(strategy -> strategiesMap.put(strategy.strategyName, strategy));
+        strategies.forEach(strategy -> strategiesMap.put(strategy.getStrategyName(), strategy));
     }
 
     public AbstractEndRegistrationSagaStrategy getStrategy(String endSagaStatus) {
